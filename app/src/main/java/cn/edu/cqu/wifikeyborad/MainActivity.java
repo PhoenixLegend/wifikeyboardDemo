1747ed4e0f000ca7e048b4f6b5a9b7b1a886cc01
unknown <>
2017-03-29 00:36:38 +0800

    first commit with UI 3/4 finished

---
+++ app/src/main/java/cn/edu/cqu/wifikeyborad/MainActivity.java
@@ -0,0 +1,101 @@
+package cn.edu.cqu.wifikeyborad;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.design.internal.NavigationMenuView;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.support.design.widget.NavigationView;
+
+import static cn.edu.cqu.wifikeyborad.R.id.drawerlayout;
+import static cn.edu.cqu.wifikeyborad.R.id.help;
+import static cn.edu.cqu.wifikeyborad.R.id.navigationView;
+import static cn.edu.cqu.wifikeyborad.R.styleable.MenuItem;
+import static cn.edu.cqu.wifikeyborad.R.styleable.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private Toolbar toolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private ListView lvLeftMenu;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar =(Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Now you see me");//设置Toolbar标题
+        toolbar.setTitleTextColor(Color.parseColor("#ffffff"));//设置标题颜色
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);//设置返回键可用
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        //创建返回键，并实现打开关/闭监听
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
+        @Override
+        public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+
+                }
+         @Override
+         public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+             }
+          };
+        mDrawerToggle.syncState();
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigationView) ;
+        if (navigationView != null) {
+            navigationView.setNavigationItemSelectedListener(this);
+        }}
+
+        android.view.MenuItem item;
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+            switch (id) {
+                case R.id.invite:
+                    mDrawerLayout.closeDrawers();
+                    break;
+
+                case R.id.help:
+                    mDrawerLayout.closeDrawers();
+                    Intent intentHelp = new Intent();
+                    intentHelp.setClass(this.getApplicationContext(),Help.class);
+                    startActivity(intentHelp);
+                    break;
+                case R.id.settings:
+                    mDrawerLayout.closeDrawers();
+                    Intent intentSettings = new Intent();
+                    intentSettings.setClass(this.getApplicationContext(),Settings.class);
+                    startActivity(intentSettings);
+                   break;
+                case R.id.quit:
+                    finishAffinity();
+                    break;
+                default:
+
+                    break;
+            }
+
+            return true;
+        }
+
+}
+
+
+
